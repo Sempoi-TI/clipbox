3f6d2cca75e86c88bf6dd223247a4961c4914a58
Elí Gamaliel Tarazona Marrujo <elitgamaliel@gmail.com>
2026-07-13 22:12:01 -0500

    feat: User and snippets separated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   build: {
     assets: '_astro',
-    assetsPrefix: './',
   },
+
   vite: {
     build: {
       assetsInlineLimit: 0,
     },
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- src/scripts/supabase-client.ts
+++ src/scripts/supabase-client.ts
@@ -4,7 +4,18 @@ const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-	throw new Error('Missing Supabase environment variables.');
+    throw new Error('Missing Supabase environment variables.');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    // PKCE es el flujo de autenticación recomendado por Supabase para SSR
+    flowType: 'pkce',
+    
+    // Evita que el servidor intente buscar tokens en la URL automáticamente
+    detectSessionInUrl: false,
+    
+    // Evita que el cliente intente persistir datos en localStorage si se ejecuta en el servidor (Node.js)
+    persistSession: typeof window !== 'undefined', 
+  },
+});
